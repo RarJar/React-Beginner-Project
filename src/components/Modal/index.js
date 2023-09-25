62b05031a74fe11dad77dc0e75a6d80e8e6de93a
Rar Zar <yarzarminkhant2003@gmail.com>
2023-09-25 20:59:59 +0630

    Portal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import ReactDom from "react-dom";
 export default function Modal({children,openModal,setOpenModal}) {
   return (
-    !!openModal && <div className='fixed inset-0 flex items-center justify-center z-50'>
+    ReactDom.createPortal(
+      !!openModal && <div className='fixed inset-0 flex items-center justify-center z-50'>
         <div className="bg-white w-1/4 p-6 h-40 rounded-lg shadow-lg">
-            <p>{children}</p>
-            <button onClick={() => setOpenModal(false)}>Close</button>
-        </div>
-    </div>
+              <p>{children}</p>
+              <button onClick={() => setOpenModal(false)}>Close</button>
+          </div>
+      </div>,
+      document.getElementById('modal')
+    )
   )
 }
